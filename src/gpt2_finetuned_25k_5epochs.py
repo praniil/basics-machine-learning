fe810cd552d0f549922a28090bebf5c37fd6592b
praniil <pparajuli15@gmail.com>
2025-01-31 15:05:43 +0545

    feat: testing the response of the model pushed in the hugging face

---
+++ src/gpt2_finetuned_25k_5epochs.py
@@ -0,0 +1,32 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from transformers import AutoConfig
+
+model_name = "Pranilllllll/finetuned-gpt2-model-124M"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+config = AutoConfig.from_pretrained(model_name)
+
+# Create a text generation pipeline
+text_generator = pipeline("text-generation", model=model_name, tokenizer=tokenizer)
+
+while True:
+    user_input = input("You: ") 
+    if user_input.lower() in ["exit", "quit"]:
+        print("Bot: Goodbye!")
+        break
+
+    inputs = tokenizer(user_input, return_tensors='pt', padding=True, truncation=True)
+
+    attention_mask = inputs['attention_mask']
+
+    response = text_generator(
+        user_input,
+        max_length=200,  
+        num_return_sequences=1,
+        do_sample=True,
+        temperature=0.7,
+        truncation=False,
+    )
+
+    suggestion = response[0]['generated_text']
+    suggestion = suggestion[len(user_input):].strip()  
+    print(f"Bot: {suggestion}")
